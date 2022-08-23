164d623cad35299223032c1006d2e3d75743bf83
aidenwalker1 <63614086+aidenwalker1@users.noreply.github.com>
2022-08-22 19:37:31 -0700

    Add files via upload

--- interactions.py
+++ interactions.py
@@ -5,47 +5,46 @@ import random
 from tokenize import Double
 import numpy
 from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
 
 from sklearn.metrics import classification_report,confusion_matrix
+
+num_features = 6
 
 
 # Import necessary modules
 from sklearn.model_selection import train_test_split
 
+#reads watch json data and converts it into clean and usable data
 def read_watch_data(bio_file, times, read_range = 10000) :
     rssi_current = 0
 
     bio_file.readline()
 
+    #output list
     bio_test = []
     
-    rssi_avg_minute = [None] * 60
-    rate_of_change = [None] * 3
-    deviations = [None] * 5
+    #data over given seconds
+    rssi_avg_minute = [0] * 60
+    rate_of_change = [0] * 3
+    deviations = [0] * 5
 
-    attribute_list = [None] * 7
+    attribute_list = [0] * (num_features + 1)
 
+    #calculated data
     roc = 0 #rate of change
     rssi_avg = 0
     stddev = 0
-
-    rate_index = 0
-    rssi_index = 0
-    dev_index = 0
     
-    time_80 = 0
-    time_70 = 0
-    time_60 = 0
-
-    dev_full = False
-    avg_full = False
-    rate_full = False
+    #current time in rssi
+    (time_60, time_70, time_80) = (0,0,0)
 
     st = ''
     fields = None
 
     interaction = 0
 
+    #goes through file
     for i in range(0,read_range) :
         #reads data, fills dictionary with json value
         st = bio_file.readline()
@@ -54,56 +53,22 @@ def read_watch_data(bio_file, times, read_range = 10000) :
         rssi_current = fields["peer_bt_rssi"] 
 
         #fills data arrays with current rssi
-        rssi_avg_minute[rssi_index] = rssi_current
-        deviations[dev_index] = rssi_current
-        rate_of_change[rate_index] = rssi_current
-
-        #updates indices for rssi, deviations, roc
-        rate_index = rate_index + 1
-        rssi_index = rssi_index + 1
-        dev_index = dev_index + 1
+        rssi_avg_minute[i % 60] = rssi_current
+        deviations[i % 5] = rssi_current
+        rate_of_change[i % 3] = rssi_current
 
         #update times based on current rssi
-        if rssi_current > -60 :
-            time_60 = time_60 + 1
-            time_70 = time_70 + 1
-        elif rssi_current > -70 :
-            time_70 = time_70 + 1
-            time_80 = time_80 + 1
-        elif rssi_current > -80 :
-            time_80 = time_80 + 1
-            time_60 = time_60 - 0.5 if time_60 > 0 else 0
-        else :
-            if rssi_current > -85 :
-                time_80 = time_80 - 1 if time_80 > 0 else 0
-                time_70 = time_70 - 0.5 if time_70 > 0 else 0
-                time_60 = 0
-            else :               
-                time_80 = 0
-                time_70 = 0
+        (time_60, time_70, time_80) = update_times(rssi_current, (time_60, time_70, time_80))
 
-        #calculates average, rate of change, std deviation
-        rssi_avg = calculate_avg(avg_full, rssi_avg_minute, rssi_index)
+        #calculates average, rate of change, standard deviation
+        rssi_avg = calculate_avg(rssi_avg_minute)
         
-        roc = calculate_roc(rate_full, rate_of_change, rate_index)
+        roc = calculate_roc(rate_of_change)
 
-        stddev = calculate_stddev(dev_full, deviations, dev_index, rssi_avg)
+        stddev = calculate_stddev(deviations, rssi_avg)
 
         #checks if interaction is happening in current time
         interaction = interaction_happened(times, i)
-        
-        #checks if array indices out of bounds
-        if dev_index > 4 :
-            dev_full = True
-            dev_index = 0
-
-        if rssi_index > 59 :
-            avg_full = True
-            rssi_index = 0
-
-        if rate_index > 2 :
-            rate_full = True
-            rate_index = 0
         
         #fills attribute list with fields
         fill_attributes(attribute_list, rssi_current, stddev, time_60, time_70, time_80, roc, interaction)
@@ -113,6 +78,29 @@ def read_watch_data(bio_file, times, read_range = 10000) :
     
     return bio_test
 
+#updates the times given the current rssi
+def update_times(rssi_current, times) :
+    time_60, time_70, time_80 = times
+    if rssi_current > -60 :
+        time_60 = time_60 + 1
+        time_70 = time_70 + 1
+    elif rssi_current > -70 :
+        time_70 = time_70 + 1
+        time_80 = time_80 + 1
+    elif rssi_current > -80 :
+        time_80 = time_80 + 1
+        time_60 = time_60 - 0.5 if time_60 > 0 else 0
+    else :
+        if rssi_current > -85 :
+            time_80 = time_80 - 1 if time_80 > 0 else 0
+            time_70 = time_70 - 0.5 if time_70 > 0 else 0
+            time_60 = 0
+        else :               
+            time_80 = 0
+            time_70 = 0
+    
+    return (time_60, time_70, time_80)
+
 #fills list with given attributes
 def fill_attributes(attribute_list, rssi_current, stddev, time_60, time_70, time_80, roc, interaction) :
     attribute_list[0] = rssi_current
@@ -121,47 +109,36 @@ def fill_attributes(attribute_list, rssi_current, stddev, time_60, time_70, time
     attribute_list[3] = time_70
     attribute_list[4] = time_80
     attribute_list[5] = roc
-    attribute_list[6] = interaction
+    attribute_list[num_features] = interaction
 
 #calculates rssi average over a minute
-def calculate_avg(avg_full, rssi_avg_minute, rssi_index) :
+def calculate_avg(rssi_avg_minute) :
     rssi_avg = 0
-    if not avg_full :
-        for j in range(0,rssi_index) :
-            rssi_avg = rssi_avg + rssi_avg_minute[j]
-        rssi_avg = rssi_avg / rssi_index
-    else :
-        for j in range(0,60) :
-            rssi_avg = rssi_avg + rssi_avg_minute[j]
-        rssi_avg = rssi_avg / 60
+    for j in range(0,60) :
+        rssi_avg = rssi_avg + rssi_avg_minute[j]
+    rssi_avg = rssi_avg / 60
+
     return rssi_avg
 
-def calculate_roc(rate_full, rate_of_change, rate_index) :
+#calculates rate of change over 3 seconds
+def calculate_roc(rate_of_change) :
     roc = 0
 
-    if not rate_full :
-        for j in range(0,rate_index - 1) :
-            roc = roc + (rate_of_change[j + 1] - rate_of_change[j])
-        roc = roc / rate_index
-    else :
-        for j in range(0,2) :
-            roc = roc + (rate_of_change[j + 1] - rate_of_change[j])
-        roc = roc / 3
+    for j in range(0,2) :
+        roc = roc + (rate_of_change[j + 1] - rate_of_change[j])
+    roc = roc / 2
+        
     return roc
 
-def calculate_stddev(dev_full, deviations, dev_index, rssi_avg):
+#calculates standard deviation over 5 seconds
+def calculate_stddev(deviations, rssi_avg):
     stddev = 0
-    if not dev_full:
-        for j in range(0, dev_index) :
-            stddev = stddev + pow(deviations[j]-rssi_avg, 2)
-    else :
-        for j in range(0, 5) :
-            stddev = stddev + pow(deviations[j]-rssi_avg, 2)
+    for j in range(0, 5) :
+        stddev = stddev + pow(deviations[j]-rssi_avg, 2)
+       
     return pow(stddev, 0.5)
 
 #detects if current time is in given interaction times
-#params: times: list of tuples of ints
-#        time: int
 def interaction_happened(times, time) :
     for cur_time in times: 
         if cur_time[0] <= time <= cur_time[1] :
@@ -169,14 +146,15 @@ def interaction_happened(times, time) :
 
     return 0 
 
+#creates model
 def train_data(bio_train) :
     arr = numpy.array(bio_train)
-    X = arr[:,0:6]
-    y = arr[:,6]
+    X = arr[:,0:num_features]
+    y = arr[:,num_features]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.003, random_state=40)
 
-    mlp = MLPClassifier(hidden_layer_sizes=(64,32,16), activation='logistic', solver='adam', max_iter=10000,batch_size=200)
+    mlp = MLPClassifier(hidden_layer_sizes=(64,32,16), activation='logistic', solver='adam', max_iter=20000,batch_size=200,tol=1e-5)
     mlp.fit(X_train,y_train)
 
     predict_train = mlp.predict(X_train)
@@ -192,20 +170,28 @@ def train_data(bio_train) :
 def test_model(model, test_arr) :
     time_since_interaction = 0
     can_prompt = False
+
+    #goes through test data
     for i in range(0,int(len(test_arr) / 8)) :
-        X = test_arr[i*8:(i*8)+8,0:6]
+        #gets 8 seconds of data and predicts if interaction occured
+        X = test_arr[i*8:(i*8)+8,0:num_features]
         pred = model.predict(X)
+
         total = 0
 
+        #goes through and sums interaction values
         for elem in pred:  
             total = total + elem
 
+        #if any second had an interaction
         if total >= 1 :
             time_since_interaction = 0
             can_prompt = True
         else:
-            time_since_interaction = time_since_interaction + 5
-            if can_prompt and time_since_interaction >= 20 :
+            time_since_interaction = time_since_interaction + 8
+
+            #prompts user after 16 seconds
+            if can_prompt and time_since_interaction >= 16 :
                 print('interaction at ' + str(i*8))
                 can_prompt = False
 
